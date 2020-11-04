038f55c1930d62d0f20329fe64f217ce86db6d4d
TheBombTNT <42681446+TheBombTNT@users.noreply.github.com>
2020-11-03 19:20:13 -0500

    User Backend
    Backend can now accept and create user to database in MongoDB

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -1,29 +1,28 @@
-import { request } from 'express'
-// import jwt from 'jsonwebtoken'
+import jwt from 'jsonwebtoken'
 import asyncHandler from 'express-async-handler'
 import User from '../models/userModel.js'
 
-const protect = asyncHandler(async (req, res, next) => {
+const protect = asyncHandler( async (req, res, next) => {
     let token
 
     if  (
-        req.headers.authorization && 
-        req.headers.authorization.startsWith('Bearer')
-        ) {
-        try {
-            token = req.headers.authorization.split(' ')[1]
+            req.headers.authorization && 
+            req.headers.authorization.startsWith('Bearer')
+        )   {
+                try {
+                    token = req.headers.authorization.split(' ')[1]
 
-            const decoded = jwt.verify(token, process.env.JWT_SECRET)
+                    const decoded = jwt.verify(token, process.env.JWT_SECRET)
 
-            req.user = await User.findById(decoded.id).select('-password')
+                    req.user = await User.findById(decoded.id).select('-password')
 
-            next()
-        } catch (error) {
-            console.error(error);
-            res.status(401)
-            throw new Error('Not authorized, token failed')
-        }
-    }
+                    next()
+                } catch (error) {
+                    console.error(error)
+                    res.status(401)
+                    throw new Error('Not authorized, token failed')
+                }
+            }
 
     if  (!token) {
         res.status(401)
